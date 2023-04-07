860cc40bc430dc3ffc619409c626a8cf03903031
Daria Okhotnikova <d_okhotnikova@mail.ru>
2023-04-07 21:55:25 +0300

    db is done

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type indexPage struct {
@@ -13,42 +15,60 @@ type indexPage struct {
 }
 
 type featuredPostData struct {
-	Title       string
-	Subtitle    string
-	ImgModifier string
-	Author      string
-	AuthorImg   string
-	PublishDate string
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	ImgModifier string `db:"image_url"`
+	Author      string `db:"author"`
+	AuthorImg   string `db:"author_url"`
+	PublishDate string `db:"publish_date"`
 }
 
 type mostRecentPostData struct {
-	Title       string
-	Subtitle    string
-	ImgPost     string
-	Author      string
-	AuthorImg   string
-	PublishDate string
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	ImgPost     string `db:"image_url"`
+	Author      string `db:"author"`
+	AuthorImg   string `db:"author_url"`
+	PublishDate string `db:"publish_date"`
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("pages/index.html")
-	if err != nil {
-		http.Error(w, "Internet Server Error", 500)
-		log.Println(err.Error())
-		return
-	}
+func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		featuredPostsData, err := featuredPosts(db)
+		if err != nil {
+			http.Error(w, "Internet Server Error", 500)
+			log.Println(err.Error())
+			return
+		}
 
-	data := indexPage{
-		Title:           "Escape",
-		FeaturedPosts:   featuredPosts(),
-		MostRecentPosts: mostRecentPosts(),
-	}
+		mostRecentPostsData, err := mostRecentPosts(db)
+		if err != nil {
+			http.Error(w, "Internet Server Error", 500)
+			log.Println(err.Error())
+			return
+		}
 
-	err = ts.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
+		ts, err := template.ParseFiles("pages/index.html")
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err.Error())
+			return
+		}
+
+		data := indexPage{
+			Title:           "Escape",
+			FeaturedPosts:   featuredPostsData,
+			MostRecentPosts: mostRecentPostsData,
+		}
+
+		err = ts.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err.Error())
+			return
+		}
+
+		log.Println("Request completed successfully")
 	}
 }
 
@@ -68,76 +88,45 @@ func post(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func featuredPosts() []featuredPostData {
-	return []featuredPostData{
-		{
-			Title:       "The Road Ahead",
-			Subtitle:    "The road ahead might be paved - it might not be.",
-			ImgModifier: "featured-post_background_the-road-ahead",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/mat_vogels.png",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "From Top Down",
-			Subtitle:    "Once a year.",
-			ImgModifier: "featured-post_background_from-top-down",
-			Author:      "William Wong",
-			AuthorImg:   "static/img/william_wong.png",
-			PublishDate: "9/25/2015",
-		},
+func featuredPosts(db *sqlx.DB) ([]featuredPostData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			author,
+			author_url,
+			publish_date,
+			image_url
+		FROM
+			post
+		WHERE featured = 1
+	`
+	var posts []featuredPostData
+
+	err := db.Select(&posts, query)
+	if err != nil {
+		return nil, err
 	}
+	return posts, nil
 }
 
-func mostRecentPosts() []mostRecentPostData {
-	return []mostRecentPostData{
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			ImgPost:     "static/img/still_standing_tall.png",
-			Author:      "William Wong",
-			AuthorImg:   "static/img/william_wong.png",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Sunne Side Up",
-			Subtitle:    "No place is ever as bad as they tell you it's going to be.",
-			ImgPost:     "static/img/sunny_side_up.png",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/mat_vogels.png",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Water Falls",
-			Subtitle:    "We travel not to escape life, but for life not to escape us.",
-			ImgPost:     "static/img/water_falls.png",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/mat_vogels.png",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Through the mist",
-			Subtitle:    "Travel makes you see what a tiny place you occupy in the world.",
-			ImgPost:     "static/img/through_the_mist.png",
-			Author:      "William Wong",
-			AuthorImg:   "static/img/william_wong.png",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Awaked Early",
-			Subtitle:    "Not all those who wander are lost.",
-			ImgPost:     "static/img/awaked_early.png",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/mat_vogels.png",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Try It Always",
-			Subtitle:    "The world is a book, and those who do not travel read only one page.",
-			ImgPost:     "static/img/try_it_always.png",
-			Author:      "Mat Vogels",
-			AuthorImg:   "static/img/mat_vogels.png",
-			PublishDate: "9/25/2015",
-		},
+func mostRecentPosts(db *sqlx.DB) ([]mostRecentPostData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			author,
+			author_url,
+			publish_date,
+			image_url
+		FROM
+			post
+		WHERE featured = 0
+	`
+	var posts []mostRecentPostData
+	err := db.Select(&posts, query)
+	if err != nil {
+		return nil, err
 	}
+	return posts, nil
 }
--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 const (
-	port = ":3000"
+	port         = ":3000"
+	dbDriverName = "mysql"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/home", index)
-	mux.HandleFunc("/post", post)
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
+	dbx := sqlx.NewDb(db, dbDriverName)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/home", index(dbx))
+	mux.HandleFunc("/post", post)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	log.Println("Start server " + port)
-	err := http.ListenAndServe(port, mux)
+	fmt.Println("Start server")
+	err = http.ListenAndServe(port, mux)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
+}
+
+func openDB() (*sql.DB, error) {
+	return sql.Open(dbDriverName, "root:Ahsad1454!!!@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
